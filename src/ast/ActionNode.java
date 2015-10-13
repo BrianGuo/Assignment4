a46cbca3f5bb22a82a18d7a5a80e3a079371ccdc
Brian <Brian@brians-macbook.home>
2015-10-12 23:06:17 -0400

    changed actionNode

--- src/ast/ActionNode.java
+++ src/ast/ActionNode.java
@@ -2,23 +2,50 @@ package ast;
 
 public class ActionNode implements Node {
 
+	String type;
+	Expr num;
+	
+	
+	public ActionNode(String t, Expr r){
+		type = t;
+		num = r;
+	}
+	
+	public ActionNode(String t) {
+		type = t;
+		num = null;
+	}
 	
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		if (num != null)
+			return num.size() +1;
+		else
+			return 1;
 	}
 
 	@Override
 	public Node nodeAt(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		if (index == 0)
+			return this;
+		if (index < 0 || (num != null && index > num.size())|| (num == null && index > 0))
+			throw new IndexOutOfBoundsException();
+		else {
+			return num.nodeAt(index-1);
+		}
 	}
 
 	@Override
 	public StringBuilder prettyPrint(StringBuilder sb) {
-		// TODO Auto-generated method stub
-		return null;
+		String possibleEnd = "";
+		if (type.equals("tag") || type.equals("serve"))
+			possibleEnd = "[" + num.toString() + "]";
+		return sb.append(type + possibleEnd);
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		return prettyPrint(sb).toString();
 	}
 	
 	
