0149837adb063a01050c54aecf540eebd31734d1
Max Zhou <ball908765@gmail.com>
2015-10-13 21:57:35 -0400

    added main skeleton code

---
+++ src/main/Driver.java
@@ -0,0 +1,40 @@
+package main;
+import ast.*;
+import parse.*;
+import exceptions.*;
+
+import java.io.File;
+import java.util.Scanner;
+//import org.apache.commons.cli.*;
+
+public class Driver {
+    public static void main(String[] args){
+        /*
+        File inputFile;
+        try {
+
+            if(args[0].equals("--mutate")){
+                int numMutations = Integer.parseInt(args[1]);
+                inputFile = new Scanner(args[2]); //change to scanner that splits on ";"
+                ProgramImpl prog = new ProgramImpl(something to do with that file);
+                for(int i = 0; i < numMutations; i++){
+                prog.mutate();
+                //print out what got changed
+                prog.prettyPrting();
+                }
+
+            }
+            else{
+                inputFile = new Scanner(args[2]);
+                ProgramImpl prog = new ProgramImpl(something to do with that file);
+                System.out.println(prog.prettyPrint());
+            }
+        }
+        catch (Exception e){
+            System.out.println("Usage:");
+            System.exit(1);
+        }
+    }
+         */
+
+}
