19cc91a1f6f382315f477d24b2b6d969f869ffe8
Max Zhou <ball908765@gmail.com>
2015-10-24 16:58:18 -0400

    added doc for coordinates

--- src/world/Coordinate.java
+++ src/world/Coordinate.java
@@ -3,6 +3,10 @@ package world;
 
 import exceptions.IllegalCoordinateException;
 
+/**
+ * A class that represents a location on the world.
+ * Class invariant: x>= 0, y >= 0
+ */
 public class Coordinate {
     private int x;
     private int y;
@@ -12,14 +16,29 @@ public class Coordinate {
         this.y = y;
     }
 
-    public void move(int destX, int destY) throws IllegalCoordinateException {
-        if(destX < 0 || destY < 0){
+    /**
+     * Preserves the class invariant of x >= 0, y >= 0
+     * @param x X coordinate of the point
+     * @param y Y coordinate of the point
+     * @throws IllegalCoordinateException
+     */
+
+    public void checkBounds(int x, int y) throws IllegalCoordinateException{
+        if(x < 0 || y < 0){
             throw new IllegalCoordinateException();
         }
-        else{
-            x = destX;
-            y = destY;
-        }
+    }
+
+    /**
+     * Changes the coordinate of an Entity from one location to another.
+     * @param destX The new X coordinate to move to
+     * @param destY The new Y coordinate to move to
+     * @throws IllegalCoordinateException if (destX, destY) is an illegal point
+     */
+    public void move(int destX, int destY) throws IllegalCoordinateException {
+        checkBounds(destX, destY);
+        x = destX;
+        y = destY;
     }
 
     public int getX() {
