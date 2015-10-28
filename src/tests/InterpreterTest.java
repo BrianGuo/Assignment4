0fee3616b3c6d62baea2c490de024cc3d0875b39
Brian <Brian@dhcp-legacy-343.eduroam.cornell.edu>
2015-10-27 22:16:25 -0400

    Tests

---
+++ src/tests/InterpreterTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InterpreterTest {
+
+	@Test
+	public void testInterpret() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEvalCondition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEvalExpr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPerform() {
+		fail("Not yet implemented");
+	}
+
+}
