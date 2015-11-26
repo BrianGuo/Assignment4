b547fae489f560db16d9590237c85202fa31bf42
Max Zhou <ball908765@gmail.com>
2015-11-26 16:12:52 -0500

    building to jar works now

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -4,7 +4,7 @@ import static spark.Spark.*;
 /**
  * Created by Max on 11/26/2015.
  */
-public class Server {
+public class Main {
 
         public static void main(String[] args) {
             get("/hello", (req, res) -> "Hello World");
